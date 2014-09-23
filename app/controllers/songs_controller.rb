981e31a0209e92fb3e7c7e57cc5bb374e813f849
Sarah Reid <pair+sarah@ent.io>
2014-09-22 18:03:17 -0700

    allow songs to be added to playlists

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -69,6 +69,6 @@ class SongsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def song_params
-      params.require(:song).permit(:title, :artist, :album, :year, :track, :length)
+      params.require(:song).permit(:title, :artist, :album, :year, :track, :length, :playlist_ids => [])
     end
 end
