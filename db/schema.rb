f5de2130c538b776d4d2ceec019ceb70769dd483
Sarah Reid <pair+sarah@ent.io>
2014-10-17 20:08:25 -0700

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140919163927) do
+ActiveRecord::Schema.define(version: 20141018025404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20140919163927) do
     t.float    "length"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "soundcloud_id"
+    t.string   "soundcloud_permalink_url"
   end
 
 end
