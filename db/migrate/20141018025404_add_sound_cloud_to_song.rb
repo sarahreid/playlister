a89d1b6c266cedab2f656614ef81b4531aa760c3
Sarah Reid <pair+sarah@ent.io>
2014-10-17 23:05:58 -0700

    set default id

--- db/migrate/20141018025404_add_sound_cloud_to_song.rb
+++ db/migrate/20141018025404_add_sound_cloud_to_song.rb
@@ -1,6 +1,6 @@
 class AddSoundCloudToSong < ActiveRecord::Migration
   def change
-    add_column :songs, :soundcloud_id, :integer
+    add_column :songs, :soundcloud_id, :integer, default: 0
     add_column :songs, :soundcloud_permalink_url, :string
   end
 end
