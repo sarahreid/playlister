d0a578dac47f71f2d143ca5524af7208e47deb01
Sarah Reid <pair+sarah@ent.io>
2014-11-27 19:49:55 -0800

    redirect existing user to login

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,10 @@
 class RegistrationsController < Devise::RegistrationsController
   def new
-    session["new_user"] = params.fetch("user").permit("email") if params.key?("user")
+    if params.key?("user")
+      new_user_params = params.fetch("user").permit("email")
+      return redirect_to new_user_session_url(params) if new_user_params["email"].present? && User.exists?(email: new_user_params["email"])
+      session["new_user"] = new_user_params
+    end
     super
   end
 end
